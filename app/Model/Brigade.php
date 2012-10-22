2d1cd6394baef896885e8d4e9146fd02ebaf99e1
Evan M. Sanders <evan@newton>
2012-10-22 13:40:51 +1100

    Added validation rules to Brigade model.

--- app/Model/Brigade.php
+++ app/Model/Brigade.php
@@ -5,6 +5,15 @@ class Brigade extends Model {
     public $name = 'Brigade';
     public $hasMany = 'Contact';
 
+    public $validate = array(
+        'name' => array(
+            'required' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a name for the Brigade.'
+            )
+        )
+    );
+
 }
 
 // eof
