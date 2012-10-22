0275de8fbdde08b415e31eac28a382758b210c15
Evan M. Sanders <evan@newton>
2012-10-22 14:21:45 +1100

    Added validation rules to Modem model.

--- app/Model/Modem.php
+++ app/Model/Modem.php
@@ -3,6 +3,30 @@
 class Modem extends AppModel {
 
     public $name = 'Modem';
+    public $hasMany = 'Contact';
+
+    public $validate = array(
+        'name' => array(
+            'required' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a name for the modem.'
+            )
+        ),
+        'device' => array(
+            'required' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a device number.'
+            ),
+            'numeric' => array(
+                'rule' => 'numeric',
+                'message' => 'The device must be a number.'
+            ),
+            'unique' => array(
+                'rule' => 'isUnique',
+                'message' => 'That device number is already in use.'
+            )
+        )
+    );
 
 }
 
