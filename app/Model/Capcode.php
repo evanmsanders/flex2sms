e56b6dae6ed5e2cd2a43d26331f75d53c5558ad0
Evan M. Sanders <evan@newton>
2012-10-22 13:49:32 +1100

    Changed displayField for Capcode model and added validation rules.

--- app/Model/Capcode.php
+++ app/Model/Capcode.php
@@ -3,7 +3,21 @@
 class Capcode extends AppModel {
 
     public $name = 'Capcode';
+    public $displayField = 'code';
     public $hasMany = 'Service';
+
+    public $validate = array(
+        'code' => array(
+            'required' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a code.'
+            ),
+            'numeric' => array(
+                'rule' => 'numeric',
+                'message' => 'The code must be a number.'
+            )
+        )
+    );
 
 }
 
