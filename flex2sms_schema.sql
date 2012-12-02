3b3f2ec6b277d6712ad490eb94ec5dace1f407bb
Evan Sanders <evan@evanmsanders.com>
2012-12-02 12:29:52 +1100

    Updated scheme file.
    1. Changed services.active to tinyint(1) to make CakePHP treat it as a
    boolean.

--- flex2sms_schema.sql
+++ flex2sms_schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: mysql.safirefighter.com
--- Generation Time: Oct 22, 2012 at 01:16 PM
+-- Generation Time: Dec 02, 2012 at 11:59 AM
 -- Server version: 5.1.53
 -- PHP Version: 5.2.17
 
@@ -25,11 +25,12 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `brigades`
 --
 
+DROP TABLE IF EXISTS `brigades`;
 CREATE TABLE IF NOT EXISTS `brigades` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -37,12 +38,13 @@ CREATE TABLE IF NOT EXISTS `brigades` (
 -- Table structure for table `capcodes`
 --
 
+DROP TABLE IF EXISTS `capcodes`;
 CREATE TABLE IF NOT EXISTS `capcodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` int(11) NOT NULL,
-  `alias` varchar(255) NOT NULL,
+  `code` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
+  `alias` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -50,6 +52,7 @@ CREATE TABLE IF NOT EXISTS `capcodes` (
 -- Table structure for table `contacts`
 --
 
+DROP TABLE IF EXISTS `contacts`;
 CREATE TABLE IF NOT EXISTS `contacts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -59,12 +62,28 @@ CREATE TABLE IF NOT EXISTS `contacts` (
   `modified` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `default_not_before` time DEFAULT '00:00:00',
-  `default_not_after` time DEFAULT '23:59:59',
+  `time_prefs_enabled` tinyint(1) NOT NULL,
+  `default_not_before` time DEFAULT NULL,
+  `default_not_after` time DEFAULT NULL,
   `approved_by` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'All contact information.',
+  `enabled` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts_services`
+--
+
+DROP TABLE IF EXISTS `contacts_services`;
+CREATE TABLE IF NOT EXISTS `contacts_services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -72,6 +91,7 @@ CREATE TABLE IF NOT EXISTS `contacts` (
 -- Table structure for table `inbox`
 --
 
+DROP TABLE IF EXISTS `inbox`;
 CREATE TABLE IF NOT EXISTS `inbox` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `number` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -81,7 +101,50 @@ CREATE TABLE IF NOT EXISTS `inbox` (
   `phone` tinyint(4) DEFAULT NULL,
   `processed` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keywords`
+--
+
+DROP TABLE IF EXISTS `keywords`;
+CREATE TABLE IF NOT EXISTS `keywords` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `word` varchar(100) NOT NULL COMMENT 'Key words',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keywords_services`
+--
+
+DROP TABLE IF EXISTS `keywords_services`;
+CREATE TABLE IF NOT EXISTS `keywords_services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `keyword_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `capcode` varchar(45) DEFAULT NULL,
+  `message` text,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Log of all pager messages.';
 
 -- --------------------------------------------------------
 
@@ -89,12 +152,13 @@ CREATE TABLE IF NOT EXISTS `inbox` (
 -- Table structure for table `modems`
 --
 
+DROP TABLE IF EXISTS `modems`;
 CREATE TABLE IF NOT EXISTS `modems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `device` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -102,6 +166,7 @@ CREATE TABLE IF NOT EXISTS `modems` (
 -- Table structure for table `multipartinbox`
 --
 
+DROP TABLE IF EXISTS `multipartinbox`;
 CREATE TABLE IF NOT EXISTS `multipartinbox` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `number` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -114,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `multipartinbox` (
   `maxnum` int(8) DEFAULT NULL,
   `curnum` int(8) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -122,6 +187,7 @@ CREATE TABLE IF NOT EXISTS `multipartinbox` (
 -- Table structure for table `outbox`
 --
 
+DROP TABLE IF EXISTS `outbox`;
 CREATE TABLE IF NOT EXISTS `outbox` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `number` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -134,8 +200,10 @@ CREATE TABLE IF NOT EXISTS `outbox` (
   `dreport` tinyint(4) NOT NULL DEFAULT '0',
   `not_before` time NOT NULL DEFAULT '00:00:00',
   `not_after` time NOT NULL DEFAULT '23:59:59',
+  `service_id` int(10) DEFAULT NULL,
+  `contact_id` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -143,34 +211,13 @@ CREATE TABLE IF NOT EXISTS `outbox` (
 -- Table structure for table `services`
 --
 
+DROP TABLE IF EXISTS `services`;
 CREATE TABLE IF NOT EXISTS `services` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `capcode_id` int(11) NOT NULL,
-  `contact_id` int(11) NOT NULL,
-  `type` VARCHAR(45) NULL,
   `comment` text COLLATE utf8_unicode_ci NOT NULL,
-  `active` INT NULL DEFAULT 1  AFTER `comment` 
+  `active` tinyint(1) DEFAULT '1',
+  `type` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `keywords`
---
-
-CREATE  TABLE IF NOT EXISTS `keywords` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `word` VARCHAR(100) NOT NULL COMMENT 'Key words' ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) );
--- --------------------------------------------------------
-
---
--- Table structure for table `keywords_services`
---
-CREATE  TABLE IF NOT EXISTS `keywords_services` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `keyword_id` INT NOT NULL ,
-  `service_id` INT NOT NULL COMMENT 'Services and keyword join table' ,
-  PRIMARY KEY (`id`) );
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
